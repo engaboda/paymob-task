25cfaffcb1f0a7ce870f8a115032fb8ff13352ae
Abdullah elkasass <abdullah.elkasass@lenmo.mobi>
2019-12-04 00:31:08 +0200

    add test for user can create post and see all posts in platform

--- home/tests.py
+++ home/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.core import mail
+from .models import PostModel
 # Create your tests here.
 User = get_user_model()
 
@@ -40,3 +41,31 @@ class UserTestCase(TestCase):
         resp = self.client.post(url, data, format='json')
         self.assertEqual(resp.status_code, 201)
         self.assertEqual(len(mail.outbox), 1)
+
+
+class PostTestCases(TestCase):
+    """
+        test user can create post or view his or another user post
+    """
+    def setUp(self):
+        self.user = User.objects.create_user(username='aboda', password='12456789/')
+
+    def test_user_can_create_post(self):
+        url = '/api/posts/'
+        data = {
+            'text': 'Welcome ya bro'
+        }
+        self.client.force_login(self.user)
+        resp = self.client.post(url, data, format='json')
+        self.assertEqual(resp.status_code, 201)
+        self.assertEqual(data.get('text'), resp.data.get('text'))
+    
+    def test_user_can_list_all_posts(self):
+        url = '/api/posts/'
+        PostModel.objects.create(user=self.user)
+        PostModel.objects.create(user=self.user)
+        PostModel.objects.create(user=self.user)
+        PostModel.objects.create(user=self.user)
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.data), 4)
